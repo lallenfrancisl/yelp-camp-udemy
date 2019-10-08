a2c1c41585e5aa0263a4940a63c7a503cff87f4c
Allen Francis <lallenfrancisl@gmail.com>
2019-10-09 02:12:30 +0530

    complete Section 28 YelpCamp: Basics  * Implement a basic layout for campgrounds and add new campgrounds page

---
+++ YelpCamp/app.js
@@ -0,0 +1,58 @@
+let express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+// Landing page
+app.get('/', (_req, res) => {
+    res.render('index');
+});
+
+// List all the campgrounds
+let campgrounds = [
+    {name: 'Sasi\'s Homestay', image: 'dino-reichmuth-5Rhl-kSRydQ-unsplash.jpg'},
+    {name: 'Damodarji\'s Pettikkada Camp', image: 'dino-reichmuth-pl1mhwMctJc-unsplash.jpg'},
+    {name: 'Subru\'s Camp', image: 'ridwan-kosasih-vXzSkC3-n5I-unsplash.jpg'},
+    {name: 'Sasi\'s Homestay', image: 'dino-reichmuth-5Rhl-kSRydQ-unsplash.jpg'},
+    {name: 'Damodarji\'s Pettikkada Camp', image: 'dino-reichmuth-pl1mhwMctJc-unsplash.jpg'},
+    {name: 'Subru\'s Camp', image: 'ridwan-kosasih-vXzSkC3-n5I-unsplash.jpg'},
+    {name: 'Sasi\'s Homestay', image: 'dino-reichmuth-5Rhl-kSRydQ-unsplash.jpg'},
+    {name: 'Damodarji\'s Pettikkada Camp', image: 'dino-reichmuth-pl1mhwMctJc-unsplash.jpg'},
+    {name: 'Subru\'s Camp', image: 'ridwan-kosasih-vXzSkC3-n5I-unsplash.jpg'},
+    {name: 'Sasi\'s Homestay', image: 'dino-reichmuth-5Rhl-kSRydQ-unsplash.jpg'},
+    {name: 'Damodarji\'s Pettikkada Camp', image: 'dino-reichmuth-pl1mhwMctJc-unsplash.jpg'},
+    {name: 'Subru\'s Camp', image: 'ridwan-kosasih-vXzSkC3-n5I-unsplash.jpg'},
+    {name: 'Sasi\'s Homestay', image: 'dino-reichmuth-5Rhl-kSRydQ-unsplash.jpg'},
+    {name: 'Damodarji\'s Pettikkada Camp', image: 'dino-reichmuth-pl1mhwMctJc-unsplash.jpg'},
+    {name: 'Subru\'s Camp', image: 'ridwan-kosasih-vXzSkC3-n5I-unsplash.jpg'}
+];
+app.get('/campgrounds', (_req, res) => {
+    res.render('campgrounds', {
+        campgrounds: campgrounds
+    });
+});
+
+// add new campground to the list
+app.post('/campgrounds', (req, res) => {
+    // get data from form and add to campgrounds list
+    let name = req.body.name, image = req.body.image;
+    campgrounds.push({name, image});
+
+    // redirect to campgrounds page
+    res.redirect('campgrounds');
+});
+
+// form for adding new campground
+app.get('/campgrounds/new', (req, res) => {
+    res.render('new-camp.ejs');
+});
+
+
+const IP = '0.0.0.0', PORT = 3000;
+app.listen(PORT, IP, () => {
+    console.log('Server has started at http://' + IP + ':' + PORT);
+});
