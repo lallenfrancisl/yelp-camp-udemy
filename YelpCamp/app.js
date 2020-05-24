bbabfbe2d071ab18ff5b6009f801302df762af5a
Allen Francis <lallenfrancisl@gmail.com>
2020-05-24 18:07:26 +0530

    complete Section 30: Data Persistance  * adds a database using mongodb to the web app

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,11 +1,24 @@
 let express = require('express'),
     app = express(),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
+
+// connect to mongo database
+mongoose.connect('mongodb://localhost/yelp_camp', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+
+// schema setup
+let campgroungSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+let Campground = mongoose.model('Campground', campgroungSchema);
 
 /**
  * Global variables
@@ -15,37 +28,23 @@ app.use(bodyParser.urlencoded({extended: true}));
 let defaultStyles = ['/css/lib/bootstrap-grid.min.css', '/css/common.css'],
     defaultScripts = ['/js/common.js'];
 
-// List all the campgrounds
-let campgrounds = [
-    {name: 'Sasi\'s Homestay', image: 'dino-reichmuth-5Rhl-kSRydQ-unsplash.jpg'},
-    {name: 'Damodarji\'s Pettikkada Camp', image: 'dino-reichmuth-pl1mhwMctJc-unsplash.jpg'},
-    {name: 'Subru\'s Camp', image: 'ridwan-kosasih-vXzSkC3-n5I-unsplash.jpg'},
-    {name: 'Sasi\'s Homestay', image: 'dino-reichmuth-5Rhl-kSRydQ-unsplash.jpg'},
-    {name: 'Damodarji\'s Pettikkada Camp', image: 'dino-reichmuth-pl1mhwMctJc-unsplash.jpg'},
-    {name: 'Subru\'s Camp', image: 'ridwan-kosasih-vXzSkC3-n5I-unsplash.jpg'},
-    {name: 'Sasi\'s Homestay', image: 'dino-reichmuth-5Rhl-kSRydQ-unsplash.jpg'},
-    {name: 'Damodarji\'s Pettikkada Camp', image: 'dino-reichmuth-pl1mhwMctJc-unsplash.jpg'},
-    {name: 'Subru\'s Camp', image: 'ridwan-kosasih-vXzSkC3-n5I-unsplash.jpg'},
-    {name: 'Sasi\'s Homestay', image: 'dino-reichmuth-5Rhl-kSRydQ-unsplash.jpg'},
-    {name: 'Damodarji\'s Pettikkada Camp', image: 'dino-reichmuth-pl1mhwMctJc-unsplash.jpg'},
-    {name: 'Subru\'s Camp', image: 'ridwan-kosasih-vXzSkC3-n5I-unsplash.jpg'},
-    {name: 'Sasi\'s Homestay', image: 'dino-reichmuth-5Rhl-kSRydQ-unsplash.jpg'},
-    {name: 'Damodarji\'s Pettikkada Camp', image: 'dino-reichmuth-pl1mhwMctJc-unsplash.jpg'},
-    {name: 'Subru\'s Camp', image: 'ridwan-kosasih-vXzSkC3-n5I-unsplash.jpg'}
-];
-
-// ------------------------------------------------------------------------------------------
-
 // Home page
 app.get('/', (_req, res) => {
     styles = [], scripts = [];
     styles.push('/css/index.css');
-    res.render('index', {
-        defaultStyles: defaultStyles,
-        styles: styles,
-        defaultScripts: defaultScripts,
-        scripts: scripts,
-        campgrounds: campgrounds
+    Campground.find({}, (err, campgrounds) => {
+        if(err) {
+            console.error(err);
+        }
+        else {
+            res.render('index', {
+                defaultStyles: defaultStyles,
+                styles: styles,
+                defaultScripts: defaultScripts,
+                scripts: scripts,
+                campgrounds: campgrounds
+            });
+        }
     });
 });
 
@@ -53,23 +52,44 @@ app.get('/', (_req, res) => {
 app.get('/campgrounds', (_req, res) => {
     styles = [], scripts = [];
     styles.push('/css/campgrounds.css')
-    res.render('campgrounds', {
-        defaultStyles: defaultStyles,
-        styles: styles,
-        defaultScripts: defaultScripts,
-        scripts: scripts,
-        campgrounds: campgrounds
+
+    Campground.find({}, (err, campgrounds) => {
+        if(err) {
+            console.error(err);
+        }
+        else {
+            res.render('campgrounds', {
+                defaultStyles: defaultStyles,
+                styles: styles,
+                defaultScripts: defaultScripts,
+                scripts: scripts,
+                campgrounds: campgrounds
+            });
+        }
     });
 });
 
 // add new campground to the list
 app.post('/campgrounds', (req, res) => {
-    // get data from form and add to campgrounds list
-    let name = req.body.name, image = req.body.image;
-    campgrounds.push({name, image});
-
-    // redirect to campgrounds page
-    res.redirect('campgrounds');
+    // create new campground in the database
+    // with the name and image url from the form
+    Campground.create(
+    {
+        name: req.body.name,
+        image: req.body.image,
+        description: req.body.description
+    },
+    (err, campground) => {
+        if(err) {
+            console.error(err);
+        }
+        else {
+            console.log('campground added');
+            console.log(campground);
+            // redirect to campgrounds page
+            res.redirect('campgrounds');
+        }
+    });
 });
 
 // form for adding new campground
@@ -81,6 +101,26 @@ app.get('/campgrounds/new', (req, res) => {
         styles: styles,
         defaultScripts: defaultScripts,
         scripts: scripts
+    });
+});
+
+// show detail about each campground
+app.get('/campgrounds/:id', (req, res) => {
+    styles = [], scripts = [];
+    styles.push('/css/show.css')
+    Campground.findById(req.params.id, (err, campground) => {
+        if(err) {
+            console.error(err);
+        }
+        else {
+            res.render('show', {
+                defaultStyles: defaultStyles,
+                styles: styles,
+                defaultScripts: defaultScripts,
+                scripts: scripts,
+                campground: campground
+            })
+        }
     });
 });
 
